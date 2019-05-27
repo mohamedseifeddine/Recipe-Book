d58d3e1ee7f1a0837c16594edbb9a754f3039e7f
hamza <hamzamohamedseifeddine@gmail.com>
2019-05-27 15:25:59 +0100

    Adding content to the recipes Component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model'
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+recipes : Recipe[] = [
+  new Recipe('TEST RECIPE ','desc ','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/portobellos-rotis-et-puree-a-la-ciboulette-935e574f.jpg')
+] ;
   constructor() { }
 
   ngOnInit() {
