6bd9920db359b73a939b4db85c417b8104377396
hamza <hamzamohamedseifeddine@gmail.com>
2019-05-27 15:52:29 +0100

    Finishing the Recipe Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe(' TEST RECIPE 1','desc ','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/portobellos-rotis-et-puree-a-la-ciboulette-935e574f.jpg') , 
+    new Recipe (' TEST RECIPE 2','desc ','https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/portobellos-rotis-et-puree-a-la-ciboulette-935e574f.jpg')
+  ] ;
+  
   constructor() { }
 
   ngOnInit() {
